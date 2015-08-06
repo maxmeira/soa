2b11c745e7efd6c7ae52a590aed61874f62d497a
MaxMeira <maxdawis@gmail.com>
2015-08-05 22:34:58 -0300

    Change Tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where.not(id: current_user.id)
+  end
+
   def show
     @post = Post.new
     @posts = @user.posts.order('created_at DESC')
